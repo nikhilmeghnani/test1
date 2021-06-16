d324ebad674b00c50e4f2b4646333dd803e1a2a8
Nikhil <nikhilcse91@gmail.com>
2021-06-16 15:42:54 +0530

    pyspark test

---
+++ pyspark_test.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+
+# from pyspark.sql import Sp
+
+
+if __name__ == "__main__":
+    print("application started")
+
+    spark = (
+        SparkSession.builder.appName("first spark with python")
+        .master("local[*]")
+        .getOrCreate()
+    )
+
+    input_file_path = (
+        "D:\\nikhil_meghnani\\ilink_work\\pycharm_workspace\\learning\\sample.txt"
+    )
+
+    tech_rdd = spark.sparkContext.textFile(input_file_path)
+
+    print(tech_rdd.collect())
